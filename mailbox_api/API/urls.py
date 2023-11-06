870fa96d80db19e2265bc2fdcbc230804cf9a83f
mkrol1997 <golotka333@gmail.com>
2023-11-06 17:55:12 +0100

    Added Mailbox Detaled URI

--- mailbox_api/API/urls.py
+++ mailbox_api/API/urls.py
@@ -1,8 +1,8 @@
-from .views import MailboxView
+from .views import MailboxView, MailboxDetailView
 from django.urls import path
 
 
 urlpatterns = [
     path('mailbox/', MailboxView.as_view()),
-    path('books/<int:pk>', MailboxView.as_view())
+    path('mailbox/<int:pk>', MailboxDetailView.as_view())
 ]
--- mailbox_api/API/views.py
+++ mailbox_api/API/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import (
     ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
 )
 
 from API.models import Mailbox
@@ -7,5 +8,10 @@ from API.serializers import MailboxSerializer
 
 
 class MailboxView(ListCreateAPIView):
+    queryset = Mailbox.objects.all()
+    serializer_class = MailboxSerializer
+
+
+class MailboxDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Mailbox.objects.all()
     serializer_class = MailboxSerializer
