074256469980f5d60907d61c67837437f3400c1a
mkrol1997 <golotka333@gmail.com>
2023-11-06 20:08:38 +0100

    Added Template URIs

--- mailbox_api/API/urls.py
+++ mailbox_api/API/urls.py
@@ -1,8 +1,10 @@
-from .views import MailboxView, MailboxDetailView
+from .views import MailboxView, MailboxDetailView, TemplateView, TemplateDetailView
 from django.urls import path
 
 
 urlpatterns = [
     path('mailbox/', MailboxView.as_view()),
-    path('mailbox/<int:pk>', MailboxDetailView.as_view())
+    path('mailbox/<int:pk>', MailboxDetailView.as_view()),
+    path('template/', TemplateView.as_view()),
+    path('template/<int:pk>', TemplateDetailView.as_view()),
 ]
--- mailbox_api/API/views.py
+++ mailbox_api/API/views.py
@@ -3,8 +3,8 @@ from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView,
 )
 
-from API.models import Mailbox
-from API.serializers import MailboxSerializer
+from API.models import Mailbox, Template
+from API.serializers import MailboxSerializer, TemplateSerializer
 
 
 class MailboxView(ListCreateAPIView):
@@ -15,3 +15,13 @@ class MailboxView(ListCreateAPIView):
 class MailboxDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Mailbox.objects.all()
     serializer_class = MailboxSerializer
+
+
+class TemplateView(ListCreateAPIView):
+    queryset = Template.objects.all()
+    serializer_class = TemplateSerializer
+
+
+class TemplateDetailView(ListCreateAPIView):
+    queryset = Template.objects.all()
+    serializer_class = TemplateSerializer
